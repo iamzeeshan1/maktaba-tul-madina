743c387cce5d58fc3a27aed27d90c8285df9dfa5
Abid <ranaabid52001@outlook.com>
2023-11-22 14:05:00 +0500

    front end changes

--- modules/sales/index.php
+++ modules/sales/index.php
@@ -5,38 +5,39 @@ include("../../includes/header.php");
 
 
 <div class="main-container container-fluid">
-  <div class="inner-body">
-    <!-- Page Header -->
-    <div class="page-header">
-      <div>
-        <h2 class="main-content-title tx-24 mg-b-5">Sales</h2>
-      </div>
-      <div class="d-flex">
-        <div class="justify-content-center">
-          <a href="create.php" class="btn btn-primary my-2 btn-icon-text">
-            <i class="fe fe-plus me-2"></i> Add Sales 
-          </a>
+    <div class="inner-body">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div>
+                <h2 class="main-content-title tx-24 mg-b-5">Sales</h2>
+            </div>
+            <div class="d-flex">
+                <div class="justify-content-center">
+                    <a href="create.php" class="btn btn-primary my-2 btn-icon-text">
+                        <i class="fe fe-plus me-2"></i> Add Sales
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <!-- End Page Header -->
-    <!-- Row -->
-    <div class="row row-sm">
-      <div class="col-xl-12 col-lg-12 col-md-12">
-        <div class="card custom-card">
-          <div class="card-body">
-                <div class="table-responsive mb-4 mt-4">
-                    <table class="table table-bordered caption-top W-100 dataTables_length datatable" id="saleTable">
-                        <thead class="table-light">
-                            <th width="4%">Sr.No</th>
-                            <th width="20%">Date</th>
-                            <th width="20%">Product ID</th>
-                            <th width="20%">Quantity Sold</th>
-                            <th width="5%">Actions</th>
-                        </thead>
-                        <tbody>
-                            
-                            <?php 
+        <!-- End Page Header -->
+        <!-- Row -->
+        <div class="row row-sm">
+            <div class="col-xl-12 col-lg-12 col-md-12">
+                <div class="card custom-card">
+                    <div class="card-body">
+                        <div class="table-responsive mb-4 mt-4">
+                            <table class="table table-striped table-bordered text-nowrap dataTable no-footer dtr-inline"
+                                id="saleTable">
+                                <thead>
+                                    <th width="4%">Sr.No</th>
+                                    <th width="20%">Date</th>
+                                    <th width="20%">Product ID</th>
+                                    <th width="20%">Quantity Sold</th>
+                                    <th width="5%">Actions</th>
+                                </thead>
+                                <tbody>
+
+                                    <?php 
                             $srNo=1;
                             $query = fetch_data($link,"SELECT
                             *, 
@@ -51,40 +52,42 @@ include("../../includes/header.php");
                                 foreach($query as $row_sol){
                                 $sales_id = $row_sol['sales_id'];
                             ?>
-                            <tr>
-                                <td><?=$srNo++?></td>
-                                <td><?= $row_sol['date'] ?></td>
-                                <td><?= $row_sol['product_id'] ?></td>
-                                <td><?= $row_sol['sold'] ?></td>
-                                
-                                <td>
-                                    <div class="dropdown">
-                                        <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <i class="mdi mdi-view-agenda-outline">Actions</i>
-                                        </a>
+                                    <tr>
+                                        <td><?=$srNo++?></td>
+                                        <td><?= $row_sol['date'] ?></td>
+                                        <td><?= $row_sol['product_id'] ?></td>
+                                        <td><?= $row_sol['sold'] ?></td>
 
-                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                            <li><a class="dropdown-item" href="create.php?sales_id=<?= $sales_id?>">
-                                                    <i class=" bx bx-edit"> Edit / View </i></a>
-                                            </li>
-                                           
-                                           
-                                        </ul>
-                                    </div>
-                                </td>
-                            </tr>
-                            
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                        <td>
+                                            <div class="dropdown">
+                                                <a href="#" role="button" id="dropdownMenuLink"
+                                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <i class="mdi mdi-view-agenda-outline">Actions</i>
+                                                </a>
+
+                                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                                    <li><a class="dropdown-item"
+                                                            href="create.php?sales_id=<?= $sales_id?>">
+                                                            <i class=" bx bx-edit"> Edit / View </i></a>
+                                                    </li>
+
+
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-          </div>
+            </div>
         </div>
-      </div>
+        <!-- End Row-->
     </div>
-    <!-- End Row-->
-  </div>
-</div> 
+</div>
 
 
 <?php include("../../includes/footer.php"); 
