b36f4cd4a79c0be5475b4e8a53c40b1e5d374276
Abid <ranaabid52001@outlook.com>
2024-02-13 15:56:42 +0500

    Chasnges

--- includes/header.php
+++ includes/header.php
@@ -44,7 +44,9 @@ $current_year  = date("Y", strtotime($today));
     <link rel="icon" href="<?=$app_path?>assets/img/brand/favicon.ico" type="image/x-icon" />
 
     <!-- Title -->
-    <title><?php if(isset($page_title) && $page_title!=""){echo $page_title;} else {echo "Dashboard - Maktaba-Tul-Madina";} ?></title>
+    <title>
+        <?php if(isset($page_title) && $page_title!=""){echo $page_title;} else {echo "Dashboard - Maktaba-Tul-Madina";} ?>
+    </title>
 
     <!-- Bootstrap css-->
     <link id="style" href="<?=$app_path?>assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
@@ -63,22 +65,22 @@ $current_year  = date("Y", strtotime($today));
     <!-- Sweet Alert css-->
     <!-- <link href="<?=$app_path?>assets/plugins/sweet-alert/sweetalert.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css" rel="stylesheet">
-    
+
     <!-- Mutipleselect css-->
     <link rel="stylesheet" href="<?=$app_path?>assets/plugins/multipleselect/multiple-select.css">
-    
+
     <!-- toaster css -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 
     <!--Bootstrap-datepicker css-->
-	<link rel="stylesheet" href="<?=$app_path?>assets/plugins/bootstrap-datepicker/bootstrap-datepicker.css">
-    
+    <link rel="stylesheet" href="<?=$app_path?>assets/plugins/bootstrap-datepicker/bootstrap-datepicker.css">
+
     <!-- Internal Datetimepicker-slider css -->
     <link href="<?=$app_path?>assets/plugins/amazeui-datetimepicker/css/amazeui.datetimepicker.css" rel="stylesheet">
-    
+
     <!-- DATA TABLE CSS -->
     <link href="<?=$app_path?>assets/plugins/datatable/css/dataTables.bootstrap5.css" rel="stylesheet" />
-    <link href="<?=$app_path?>assets/plugins/datatable/css/buttons.bootstrap5.min.css"  rel="stylesheet">
+    <link href="<?=$app_path?>assets/plugins/datatable/css/buttons.bootstrap5.min.css" rel="stylesheet">
     <link href="<?=$app_path?>assets/plugins/datatable/css/responsive.bootstrap5.css" rel="stylesheet" />
 </head>
 
@@ -98,25 +100,32 @@ $current_year  = date("Y", strtotime($today));
                     <a class="main-header-menu-icon" href="javascript:void(0)" id="mainSidebarToggle"><span></span></a>
                     <div class="hor-logo">
                         <a class="main-logo" href="index.html">
-                            <img src="<?=$app_path?>assets/img/brand/logo.png" class="header-brand-img desktop-logo" alt="logo">
-                            <img src="<?=$app_path?>assets/img/brand/logo-light.png" class="header-brand-img desktop-logo-dark" alt="logo">
+                            <img src="<?=$app_path?>assets/img/brand/logo.png" class="header-brand-img desktop-logo"
+                                alt="logo">
+                            <img src="<?=$app_path?>assets/img/brand/logo-light.png"
+                                class="header-brand-img desktop-logo-dark" alt="logo">
                         </a>
                     </div>
                 </div>
                 <div class="main-header-center">
                     <div class="responsive-logo">
-                        <a href="index.html"><img src="<?=$app_path?>assets/img/brand/logo.png" class="mobile-logo" alt="logo"></a>
-                        <a href="index.html"><img src="<?=$app_path?>assets/img/brand/logo-light.png" class="mobile-logo-dark" alt="logo"></a>
+                        <a href="index.html"><img src="<?=$app_path?>assets/img/brand/logo.png" class="mobile-logo"
+                                alt="logo"></a>
+                        <a href="index.html"><img src="<?=$app_path?>assets/img/brand/logo-light.png"
+                                class="mobile-logo-dark" alt="logo"></a>
                     </div>
                 </div>
                 <div class="main-header-right">
-                    <button class="navbar-toggler navresponsive-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler navresponsive-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4"
+                        aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fe fe-more-vertical header-icons navbar-toggler-icon"></i>
                     </button><!-- Navresponsive closed -->
-                    <div class="navbar navbar-expand-lg  nav nav-item  navbar-nav-right responsive-navbar navbar-dark  ">
+                    <div
+                        class="navbar navbar-expand-lg  nav nav-item  navbar-nav-right responsive-navbar navbar-dark  ">
                         <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
                             <div class="d-flex order-lg-2 ms-auto">
-                                 <!-- Notification -->
+                                <!-- Notification -->
                                 <?php
                                 // if($_SESSION['mktb_role_id']=='1'):
                                 //     $get = fetch_data($link, "SELECT count(notif_id) as total FROM notifications WHERE status='0'");
@@ -150,7 +159,7 @@ $current_year  = date("Y", strtotime($today));
                                 </div>
                                 <?//php  endif;?> -->
 
-								<!-- Notification -->
+                                <!-- Notification -->
                                 <!-- Theme-Layout -->
                                 <div class="dropdown d-flex main-header-theme">
                                     <a class="nav-link icon layout-setting">
@@ -163,7 +172,7 @@ $current_year  = date("Y", strtotime($today));
                                     </a>
                                 </div>
                                 <!-- Theme-Layout -->
-                               
+
                                 <!-- Full screen -->
                                 <div class="dropdown ">
                                     <a class="nav-link icon full-screen-link">
@@ -175,11 +184,13 @@ $current_year  = date("Y", strtotime($today));
                                 <!-- Profile -->
                                 <div class="dropdown main-profile-menu">
                                     <a class="d-flex" href="javascript:void(0)">
-                                        <span class="main-img-user"><img alt="avatar" src="<?=$app_path?>assets/img/users/1.jpg"></span>
+                                        <span class="main-img-user"><img alt="avatar"
+                                                src="<?=$app_path?>assets/img/users/1.jpg"></span>
                                     </a>
                                     <div class="dropdown-menu">
                                         <div class="header-navheading">
-                                            <h6 class="main-notification-title"><?=$_SESSION['mktb_user_displayname']?></h6>
+                                            <h6 class="main-notification-title"><?=$_SESSION['mktb_user_displayname']?>
+                                            </h6>
                                             <p class="main-notification-text">Web Designer</p>
                                         </div>
                                         <a class="dropdown-item border-top" href="profile.html">
--- includes/navigation.php
+++ includes/navigation.php
@@ -4,14 +4,15 @@
         <div class="main-sidebar-header main-container-1 active">
             <div class="sidemenu-logo">
                 <a class="main-logo" href="index.html">
-                    <img src="<?=$app_path?>assets/img/brand/logo-light.png" class="header-brand-img desktop-logo"
+                    <!-- <img src="<?=$app_path?>assets/img/brand/logo-light.png" class="header-brand-img desktop-logo"
                         alt="logo">
                     <img src="<?=$app_path?>assets/img/brand/icon-light.png" class="header-brand-img icon-logo"
                         alt="logo">
                     <img src="<?=$app_path?>assets/img/brand/logo.png" class="header-brand-img desktop-logo theme-logo"
                         alt="logo">
                     <img src="<?=$app_path?>assets/img/brand/icon.png" class="header-brand-img icon-logo theme-logo"
-                        alt="logo">
+                        alt="logo"> -->
+                    MTM
                 </a>
             </div>
             <div class="main-sidebar-body main-body-1">
