a0aa4adc8eb685639ad04eea57adcae0b080aaa9
Abid <ranaabid52001@outlook.com>
2024-02-13 15:57:36 +0500

    changes

--- modules/dashboard/index.php
+++ modules/dashboard/index.php
@@ -13,19 +13,6 @@ include("../../includes/header.php");
                     <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
                 </ol>
             </div>
-            <div class="d-flex">
-                <div class="justify-content-center">
-                    <button type="button" class="btn btn-white btn-icon-text my-2 me-2">
-                        <i class="fe fe-download me-2"></i> Import
-                    </button>
-                    <button type="button" class="btn btn-white btn-icon-text my-2 me-2">
-                        <i class="fe fe-filter me-2"></i> Filter
-                    </button>
-                    <button type="button" class="btn btn-primary my-2 btn-icon-text">
-                        <i class="fe fe-download-cloud me-2"></i> Download Report
-                    </button>
-                </div>
-            </div>
         </div>
         <!-- End Page Header -->
     </div>
